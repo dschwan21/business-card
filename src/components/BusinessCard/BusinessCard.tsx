e926f5fecacbccd49ba71d514189734fcdcd5082
Derek Schwantner <derek@Dereks-Mac-mini.local>
2023-01-25 19:05:59 -0500

    created navbar and basic filestructure with google login

---
+++ src/components/BusinessCard/BusinessCard.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface BusinessCardProps {}
+
+const BusinessCard: FC<BusinessCardProps> = ({}) => {
+    return <div>BusinessCard</div>;
+    };
+
+export default BusinessCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { data: sessionData } = useSession();
   console.log('data', sessionData);
   // const signOut = async () => {
@@ -19,7 +18,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex h-[calc(100vh-4rem)] flex-col items-center justify-center bg-gradient-to-br from-rose-500 to purple-600">
-        <h1 className="text-4xl font-bold text-white">Welcome to T3 App!</h1>
+        <h1 className="text-4xl font-bold text-white">Welcome to T3 App Derek!</h1>
         {!sessionData && (
           <button className="rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-black/20" onClick={sessionData ? () => signOut() : () => signIn('google')}>sign in with Google</button>
         )}
